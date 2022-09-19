2e73b627cd9be523057531764bf49d59ec6c3ebf
Darshan2059 <W0795110@myscc.ca>
2022-09-19 13:42:29 -0400

    S

--- WebApplication1/Models/Movie.cs
+++ WebApplication1/Models/Movie.cs
@@ -4,9 +4,16 @@ using System.Linq;
 using System.Threading.Tasks;
 */
 
-namespace WebApplication1.Models
+namespace MvcMovie.Models
 {
     public class Movie
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
     }
 }
