386fe7ec27a8fcb6924077eb25e3be380a06ac27
W0795110 <W0795110@ADSOL0045L-28>
2022-09-19 12:33:48 -0400

    Add project files.

---
+++ WebApplication1/Controllers/HelloWorldController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        //Get : /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // Get: /HelloWorld/Welcome/
+        //
+
+
+        //Requires using System.Text.Encoding.Web;
+
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
---
+++ WebApplication1/ReadMe.txt
@@ -0,0 +1,5 @@
+﻿2022-09-19
+Restarted the default Asp.Net MVC Appn
+w/ Individual auth / Http
+Tweaked the launchsetting.json due to lab restriction
+tested the Index.cshtml HELLO WORLD
